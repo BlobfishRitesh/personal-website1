52414b4051e7df6d23f071fe673a7bff3059faf2
BlobfishRitesh <90977527+BlobfishRitesh@users.noreply.github.com>
2026-06-02 10:19:25 -0400

    Minor UI and UX tweaks
    Changed the button look and the nav bar to make it look more professional and smoother

--- script.js
+++ script.js
@@ -6,17 +6,17 @@ if (darkModeBtn) {
     const saved = localStorage.getItem("theme");
     if (saved === "dark") {
         body.classList.add("dark");
-        darkModeBtn.textContent = "Light Mode Toggle ☀️";
+        darkModeBtn.textContent = "Dark Mode: On 🌙";
     } else {
-        darkModeBtn.textContent = "Dark Mode Toggle 🌙";
+        darkModeBtn.textContent = "Dark Mode: Off ☀️";
     }
     darkModeBtn.addEventListener("click", () => {
         body.classList.toggle("dark");
         if (body.classList.contains("dark")) {
-            darkModeBtn.textContent = "Light Mode Toggle ☀️";
+            darkModeBtn.textContent = "Dark Mode: On 🌙";
             localStorage.setItem("theme", "dark");
         } else {
-            darkModeBtn.textContent = "Dark Mode Toggle 🌙";
+            darkModeBtn.textContent = "Dark Mode: Off ☀️";
             localStorage.setItem("theme", "light");
         }
     });
